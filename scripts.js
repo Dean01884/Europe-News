8ef53b92f2dffb47a64bef83ba2b33c60aa4b166
Dean01884 <flemingdean016@gmail.com>
2025-01-02 03:42:56 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    const slider = document.querySelector('.slider');
+    slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    showSlide(currentIndex);
+}
+
+// Auto-slide every 5 seconds
+setInterval(nextSlide, 5000);
